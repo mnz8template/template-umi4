bc727e8af66781110f40b9fc9018268ae216d401
line126 <line112266@126.com>
2023-04-29 00:26:17 +0800

    updated create-umi@4.0.67

--- .umirc.ts
+++ .umirc.ts
@@ -2,11 +2,6 @@ import { defineConfig } from 'umi';
 
 export default defineConfig({
   npmClient: 'pnpm',
-  plugins: ['@umijs/plugins/dist/request', '@umijs/plugins/dist/antd'],
-  antd: {},
-  request: {},
-  deadCode: {},
-  clickToComponent: {},
   // proxy: {
   //   '/xxx': {
   //     target: 'xxx',
