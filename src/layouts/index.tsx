3c1537286f04138518d4fa9ac1dc3f8b68e36cea
line126 <line112266@126.com>
2023-05-18 23:11:04 +0800

    feat: 增加对动态路由处理

--- src/layouts/index.tsx
+++ src/layouts/index.tsx
@@ -1,4 +1,4 @@
-import { Link, Outlet, useAppData, useLocation } from 'umi';
+import { Link, Outlet, useAppData, useLocation, useParams } from 'umi';
 import { Layout, Menu } from 'antd';
 
 const { Content, Sider } = Layout;
@@ -7,9 +7,15 @@ const UmiLayout: React.FC = () => {
   const { routes } = useAppData();
   const collator = new Intl.Collator('en');
   const location = useLocation();
+  // 动态路由处理
+  const p = useParams();
+  const p_k_length = Object.keys(p).length;
+  const l_p_s = location.pathname.split('/');
+  const l_p_s_p = l_p_s.slice(0, l_p_s.length - p_k_length).join('/');
 
   const items = Object.entries(routes)
     .filter(([, value]) => !(value.id === '@@/global-layout' || value.id === '404'))
+    .filter(([key]) => !key?.includes('$')) // 过滤动态路由
     .sort(([x], [y]) => collator.compare(x, y))
     .map(([, value]) => {
       if (value.path === '/') {
@@ -27,7 +33,7 @@ const UmiLayout: React.FC = () => {
   return (
     <Layout style={{ minHeight: '100vh' }}>
       <Sider theme="light">
-        <Menu mode="inline" items={items} defaultSelectedKeys={[location.pathname]} />
+        <Menu mode="inline" items={items} defaultSelectedKeys={[p_k_length > 0 ? l_p_s_p : location.pathname]} />
       </Sider>
       <Content style={{ margin: '0 16px' }}>
         <Outlet />
